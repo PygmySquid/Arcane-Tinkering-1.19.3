fae4c72c7b1286f0b2c6e00433abf50318fc7694
PygmySquid <brandonmalak@gmail.com>
2023-01-04 18:50:37 -0500

    g

--- src/main/java/net/pygmysquid/arcanetinkering/ArcaneTinkering.java
+++ src/main/java/net/pygmysquid/arcanetinkering/ArcaneTinkering.java
@@ -12,7 +12,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(ArcaneTinkering.MOD_ID)
 public class ArcaneTinkering {
     public static final String MOD_ID = "arctink";
