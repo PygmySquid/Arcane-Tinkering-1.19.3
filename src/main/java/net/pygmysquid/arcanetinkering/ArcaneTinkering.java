53f2800653c4c018bca94506bd3cc83fab814d96
Brandon <brandonmalak@gmail.com>
2023-01-04 18:38:01 -0500

    g

--- src/main/java/net/pygmysquid/arcanetinkering/ArcaneTinkering.java
+++ src/main/java/net/pygmysquid/arcanetinkering/ArcaneTinkering.java
@@ -15,7 +15,6 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ArcaneTinkering.MOD_ID)
 public class ArcaneTinkering {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "arcanetinkering";
     private static final Logger LOGGER = LogUtils.getLogger();
 
